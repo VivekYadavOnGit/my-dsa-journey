d57c5e67835412adb90920318917b296aec0a1d8
VivekYadavOnGit <vivekyadavatwork@gmail.com>
2026-03-06 13:12:37 +0530

    TwoDArrays Problems

---
+++ src/com/vivek/arrays/TwoDArrays.java
@@ -0,0 +1,34 @@
+package com.vivek.arrays;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TwoDArrays {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int [][] arr = new int [3][3];
+
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = in.nextInt();
+            }
+        }
+
+
+//       for (int row = 0; row < arr.length; row++) {                //Output Type 1
+//        for (int col = 0; col < arr[row].length; col++) {
+//            System.out.print(arr[row][col]+" ");
+//         }
+//         System.out.println();
+//        }
+
+
+//        System.out.println(Arrays.deepToString(arr));             //Output Type 2
+
+
+        for(int[] num : arr){                                       // Output Type 3
+            System.out.println(Arrays.toString(num));
+        }
+    }
+}
